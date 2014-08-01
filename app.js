fb9ef5bb73133ae16ff63542f36e18460c0d035d
mnerandzic <Marko.Nerandzic@oicr.on.ca>
2014-08-01 10:56:33 -0400

    Started work on seperate pages for each conference

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var Review = require('./models/conference_review');
 var routes = require('./routes/index');
 var reviews = require('./routes/reviews');
+var conferences = require('./routes/conferences')
 
 var app = express();
 var mongoose = require('mongoose');
@@ -31,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/review', reviews);
+app.use('/conference', conferences);
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
---
+++ routes/conferences.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var Review = require('../models/conference_review');
+var reviewLandpageURL = 'http://localhost:3000/';
+
+router.get('/:name', function(req, res){
+	console.log(req.params.name)
+	Review.find({'conference_name':req.params.name}, function(err, reviewData){
+		console.log(reviewData);
+		var numOfEntries = 0;
+		var totalRating = 0;
+		reviewData.forEach(function(data) {
+			numOfEntries += 1;
+			totalRating += data.conference_rating;
+			console.log(data);
+		});
+		console.log(totalRating);
+		console.log(numOfEntries);
+		console.log(totalRating/numOfEntries);
+		res.render('conference', {reviews: reviewData, conferenceName: req.params.name, averageRating: totalRating/numOfEntries});
+	});
+});
+
+module.exports = router;
