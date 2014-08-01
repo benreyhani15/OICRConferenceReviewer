c164c05c05e749d396d1602622d0f05d635c587d
Ben Reyhani <benreyhani15@hotmail.com>
2014-08-01 00:49:32 -0400

    added pat conferences with ajax request that loads corresponding hyperlinks of conference names that point to conference/conference_name

---
+++ public/javascripts/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  /* if (window.location.hash!=""){
+     sendAJAXForReviews(window.location.hash);
+   }*/
+   $('a').click(function(){
+   	 console.log($(this).attr('href'));
+     sendAJAXForReviews($(this).attr('href'));
+   });
+
+   function sendAJAXForReviews(year){
+   	var mYear = year.substring(1,year.length);
+   	var url = "./getReviews/" + mYear;
+   	var htmlElementsArray = [];
+   	console.log(url);
+     $.getJSON(url, function(conferences){
+     	conferences.forEach(function(conference){
+     		var tmpElement = '<a href="conference/'+conference+'"> <h5>'+conference+'</h5> </a>';
+     		htmlElementsArray.push(tmpElement);
+     	});
+     	$('.col-xs-9').empty().append(htmlElementsArray);
+
+     });
+   }
+});
--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,47 @@ var express = require('express');
 var router = express.Router();
 var Review = require('../models/conference_review');
 var async = require('async');
+var url = require('url');
 
 /* GET home page. */
+
+router.get('/getReviews/:year',function(req,res){
+	var year = req.params.year;
+	Review.find({conference_year:year}).distinct('conference_name',function(err, conferences){
+		console.log(conferences);
+		res.send(conferences);
+	});
+});
+
 router.get('/', function(req, res) {
-  async.parallel([
+  var JSONArray = [];
+  async.series([
   	function(callback){
-  		Review.find({conference_year:2014},function(err,reviews){
-  			callback(err,reviews);
-  		});
-  	},
-  	function(callback){
-  		Review.find({conference_year:2013},function(err,reviews){
-  			callback(err,reviews);
-  		});
-  	},
-  	function(callback){
-  		Review.find({conference_year:2012},function(err,reviews){
-  			callback(err,reviews);
-  		});
-  	}
-  	],
-  	function(err,reviewsArray){
-      res.render('index', { title: 'OICR Conferences', reviews:reviewsArray});
-  	});
+  		Review.distinct('conference_year',{},function(err,conferenceYears){
+  			callback(err,conferenceYears.sort(function(a, b){return b-a}));
+  		})
+  	}],
+
+  	function(err,years){
+  		var allYears;
+  		console.log(years);
+  		if (err){
+  			console.log('there was error');
+  		}
+  		else {
+  		  allYears = years[0];
+  		  async.each(allYears,
+  		  	function(year,callback){
+  		  		Review.find({'conference_year': year}, function(err,reviews){
+  		  		  JSONArray.push(reviews);
+  		  	      callback(err);
+  		  		});
+  		  },
+  		  function(err){
+  		  	console.log(allYears);
+  		  	res.render('index', {reviews:JSONArray, years:allYears});
+  		  });
+  		}
+  });
 });
 module.exports = router;
